9b064ecab21109bc9752c7a19540208eb1bb5578
dearLu <lady.barabanovapolina@list.ru>
2020-09-29 19:04:55 +0400

    feat: add min, max, avg function

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
+exports.min = function min(array) {
+    let min;
 
-exports.min = function min (array) {
-  return 0;
-}
+    if (!isArray(array)) return 0;
 
-exports.max = function max (array) {
-  return 0;
-}
+    for (let i = 0; i < array.length; i++) {
+        if (typeof min === "undefined") {
+            min = array[i];
+        }
+        if (min > array[i + 1]) {
+            min = array[i + 1];
+        }
+    }
 
-exports.avg = function avg (array) {
-  return 0;
+    return min;
+};
+
+exports.max = function max(array) {
+    let max;
+
+    if (!isArray(array)) return 0;
+
+    for (let i = 0; i < array.length; i++) {
+        if (typeof max === "undefined") {
+            max = array[i];
+        }
+        if (max < array[i + 1]) {
+            max = array[i + 1];
+        }
+    }
+
+    return max;
+};
+
+exports.avg = function avg(array) {
+    let sum = 0;
+
+    if (!isArray(array)) return 0;
+
+    for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+    }
+    let result = sum / array.length;
+
+    return result;
+};
+
+function isArray(array) {
+    if (Array.isArray(array) && array.length) {
+        return true;
+    }
+    return false;
 }
